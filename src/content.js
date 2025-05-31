5e2373b9604ddca3e7d935a11678a3278d660813
Priyanshu Kumawat <priyanshukumawat434@gmail.com>
2025-05-31 18:03:16 +0530

    Add reply functionality to shadow btn

--- src/content.js
+++ src/content.js
@@ -3,3 +3,22 @@ icon.src = chrome.runtime.getURL("assets/icon-512.png");
 icon.alt = "tweet-feed-icon";
 icon.id = "shadow-icon";
 document.body.appendChild(icon);
+
+icon.addEventListener("click", () => {
+	const tweetElement = document.querySelector('[data-testid="tweetText"]');
+	const tweetText = tweetElement.querySelector("span").textContent.trim();
+	alert(tweetText);
+	writeTweetReply(tweetText);
+});
+
+/**
+ * @param {string} reply
+ * @returns {void}
+ */
+const writeTweetReply = (reply) => {
+	const tweetBox = document.querySelector('[data-testid="tweetTextarea_0"]');
+	if (tweetBox) {
+		tweetBox.focus();
+		document.execCommand("insertText", false, reply);
+	}
+};
